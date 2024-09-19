06838ee1d192b2292452a7f90bafb59e6a5ec6da
DamienTian <tian4damien@gmail.com>
2024-09-19 16:36:13 -0700

    71 unresolved

---
+++ DataStructures/Stack/71.simplify-path.cpp
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=71 lang=cpp
+ *
+ * [71] Simplify Path
+ */
+
+// @lc code=start
+//#include "dummyHead.h"
+
+// UNRESOLVED
+//  ref: https://www.cnblogs.com/grandyang/p/4347125.html
+class Solution {
+public:
+    string simplifyPath(string path) {
+        
+    }
+};
+// @lc code=end
+
