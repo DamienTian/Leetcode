31b81f1c1ecf8505b21c95842505a181a881b551
Hao Tian <tian4damien@gmail.com>
2024-09-19 20:59:47 -0700

    stack 71

--- DataStructures/Stack/71.simplify-path.cpp
+++ DataStructures/Stack/71.simplify-path.cpp
@@ -5,14 +5,29 @@
  */
 
 // @lc code=start
-//#include "dummyHead.h"
+// #include "../dummyHead.h"
 
-// UNRESOLVED
+// Solution: Stack (treat vector as stack)
 //  ref: https://www.cnblogs.com/grandyang/p/4347125.html
 class Solution {
 public:
     string simplifyPath(string path) {
-        
+        stringstream ss(path);
+        string result, current;
+        vector<string> dirs;
+        while (getline(ss, current, '/')) {
+            if(current == "." || current == "") 
+                continue;
+            if(current == ".." && !dirs.empty()){
+                dirs.pop_back();
+            }
+            else if(current != "..")
+                dirs.push_back(current);
+        }
+        for(auto d: dirs){
+            result += "/" + d;
+        }
+        return result.empty() ? "/" : result;
     }
 };
 // @lc code=end
